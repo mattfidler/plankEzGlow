a433bd7a29218704b48064acae6a69cfc151f4ee
Matthew Fidler <514778+mattfidler@users.noreply.github.com>
2021-07-26 23:15:18 -0500

    Look at and handle the top 50 English bigrams, adding rollover execptions

--- keymap.c
+++ keymap.c
@@ -264,6 +264,48 @@ uint16_t get_tapping_term(uint16_t keycode, keyrecord_t *record) {
   }
 }
 
+// http://norvig.com/mayzner.html
+// th = 3.56% (Alternating hand bigram)
+// he = 3.07% (H is not home row)
+// in = 2.43%
+// er = 2.05% Alternating hand
+// an = 1.99% Alternating hand
+// re = 1.85% Alternating hand
+// on = 1.76%
+// at = 1.49%
+// en = 1.45%
+// nd = 1.35% Alternating hand
+// ti = 1.34% Alternating hand
+// es = 1.34% Alternating hand
+// or = 1.28% Alternating hand
+// te = 1.20% Alternating hand
+// of = 1.17% Alternating hand
+// ed = 1.17% Alternating hand
+// is = 1.13% Alternating hand
+// it = 1.12% Alternating hand
+// al = 1.09% Alternating hand
+// ar = 1.07%
+// st = 1.05%
+// to = 1.04% Alternating hand
+// nt = 1.04% Alternating hand
+// ng = 0.95% Alternating hand
+// se = 0.93% Alternating hand
+// ha = 0.93% Alternating hand
+// as = 0.87%
+// ou = 0.87%
+// io = 0.83%
+// le = 0.83% no home row mods
+// ve = 0.83% no home row mods
+// co = 0.79% alternating hands
+// me = 0.79% no home row mods
+// de = 0.76% no howe row mods
+// hi = 0.73% no home row mods
+// ri = 0.73% Alternating hands
+// ro = 0.73% Alternating hands
+// ic = 0.70% Alternating hands
+// ne = 0.69%
+
+// Bigrams that do not exist jq, qg, qy, qz, wq, and qz
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
   case RGB_SLD:
@@ -273,6 +315,18 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     return false;
     // common bigraphs that contain home row mods on same hand
     // ou, in, st, en
+  case HM_R:
+    if (record->tap.count > 0) {
+      if (get_mods() & MOD_BIT(KC_LGUI)) {
+	// ar 1.07%
+	unregister_mods(MOD_BIT(KC_LGUI));
+	tap_code(KC_A);
+	tap_code(KC_R);
+	add_mods(MOD_BIT(KC_LGUI));
+	return false;
+      }
+    }
+    return true;
   case KC_M:
     // om bigraph
     if (record->tap.count > 0) {
@@ -282,6 +336,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 	tap_code(KC_M);
 	add_mods(MOD_BIT(KC_RGUI));
 	return false;
+      } else if (get_mods() & MOD_BIT(KC_RCTL)) {
+	// em bigraph
+	unregister_mods(MOD_BIT(KC_RCTL));
+	tap_code(KC_E);
+	tap_code(KC_M);
+	add_mods(MOD_BIT(KC_RCTL));
+	return false;
       }
     }
     return true;
@@ -289,6 +350,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // handle ou case and send ou for gui right+u
     if (record->tap.count > 0) {
       if (get_mods() & MOD_BIT(KC_RGUI)) {
+	// ou = 0.87%
 	unregister_mods(MOD_BIT(KC_RGUI));
 	tap_code(KC_O);
 	tap_code(KC_U);
@@ -300,7 +362,15 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   case HM_T:
     // handles st
     if (record->tap.count > 0) {
-      if (get_mods() & MOD_BIT(KC_LCTL)) {
+      if (get_mods() & MOD_BIT(KC_LGUI)) {
+	// handle at 1.49%
+	unregister_mods(MOD_BIT(KC_LGUI));
+	tap_code(KC_A);
+	tap_code(KC_T);
+	unregister_mods(MOD_BIT(KC_LGUI));
+	return false;
+      } else if (get_mods() & MOD_BIT(KC_LCTL)) {
+	// handle st 1.05%
 	unregister_mods(MOD_BIT(KC_LCTL));
 	tap_code(KC_S);
 	tap_code(KC_T);
@@ -313,27 +383,79 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     // handle en and in
     if (record->tap.count > 0) {
       uint8_t current_mods = get_mods();
-      if (current_mods & MOD_BIT(KC_RCTL)) {
-	unregister_mods(MOD_BIT(KC_RCTL));
-	tap_code(KC_E);
-	tap_code(KC_N);
-	add_mods(MOD_BIT(KC_RCTL));
-	return false;
-      } else if (current_mods & MOD_BIT(KC_RALT)) {
+      if (current_mods & MOD_BIT(KC_RALT)) {
+	// in 2.43%
 	unregister_mods(MOD_BIT(KC_RALT));
 	tap_code(KC_I);
 	tap_code(KC_N);
 	add_mods(MOD_BIT(KC_RALT));
 	return false;
       } else if (current_mods & MOD_BIT(KC_RGUI)) {
+	// on 1.76%
 	unregister_mods(MOD_BIT(KC_RGUI));
 	tap_code(KC_O);
 	tap_code(KC_N);
 	add_mods(MOD_BIT(KC_RGUI));
 	return false;
+      } else if (current_mods & MOD_BIT(KC_RCTL)) {
+	// en 1.45%
+	unregister_mods(MOD_BIT(KC_RCTL));
+	tap_code(KC_E);
+	tap_code(KC_N);
+	add_mods(MOD_BIT(KC_RCTL));
+	return false;
       }
     }
-    return true; 
+    
+    return true;
+  case HM_S:
+    if (record->tap.count > 0) {
+      if (get_mods() & MOD_BIT(KC_LGUI)) {
+	// as 0.87%
+	unregister_mods(MOD_BIT(KC_LGUI));
+	tap_code(KC_A);
+	tap_code(KC_S);
+	add_mods(MOD_BIT(KC_LGUI));
+	return false;
+      }
+      return true;
+    }
+  case HM_O:
+    if (record->tap.count > 0) {
+      if (get_mods() & MOD_BIT(KC_LALT)) {
+	// handle io 0.83%
+	unregister_mods(MOD_BIT(KC_LALT));
+	tap_code(KC_I);
+	tap_code(KC_O);
+	add_mods(MOD_BIT(KC_LALT));
+	return false;
+      }
+    }
+    return true;
+  case HM_E:
+    if (record->tap.count > 0) {
+      if (get_mods() & MOD_BIT(KC_RSHIFT)) {
+	// ne
+	unregister_mods(MOD_BIT(KC_RSHIFT));
+	tap_code(KC_N);
+	tap_code(KC_E);
+	add_mods(MOD_BIT(KC_RSHIFT));
+	return false;
+      }
+    }
+    return true;
+  case HM_A:
+    if (record->tap.count > 0) {
+      if (get_mods() & MOD_BIT(KC_RALT)) {
+	// ra
+	unregister_mods(MOD_BIT(KC_RALT));
+	tap_code(KC_R);
+	tap_code(KC_A);
+	add_mods(MOD_BIT(KC_RALT));
+	return false;
+      }
+    }
+    return true;
   }
   return true;
 }
@@ -558,12 +680,12 @@ void on_dance_k(qk_tap_dance_state_t *state, void *user_data) {
 }
 
 void dance_k_finished(qk_tap_dance_state_t *state, void *user_data) {
-    dance_state[3].step = dance_step(state);
-    switch (dance_state[3].step) {
-        case SINGLE_TAP: register_code16(KC_K); break;
-        case DOUBLE_TAP: register_code16(KC_APPLICATION); break;
-        case DOUBLE_SINGLE_TAP: tap_code16(KC_K); register_code16(KC_K);
-    }
+  dance_state[3].step = dance_step(state);
+  switch (dance_state[3].step) {
+  case SINGLE_TAP: register_code16(KC_K); break;
+  case DOUBLE_TAP: register_code16(KC_APPLICATION); break;
+  case DOUBLE_SINGLE_TAP: register_code16(KC_APPLICATION);
+  }
 }
 
 void dance_k_reset(qk_tap_dance_state_t *state, void *user_data) {
